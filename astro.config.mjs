27df506e84d100d7f98f2f94451e8951974486e0
semih babacan huawei <m.semihbabacan@gmail.com>
2024-07-21 15:33:44 +0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ const env = loadEnv("", process.cwd(), ["STORYBLOK", "NETLIFY"]);
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrorante.unfolding.io/",
+  site: "https://purewoodcreations.com/",
   output: "hybrid",
   adapter: env.NETLIFY ? netlify() : vercel(), // vercel() or netlify()
   integrations: [
